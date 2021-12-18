0292f752c20aeab64f15f345da8b355897d8aa18
wirehack7 <markusfritz87@gmail.com>
2021-12-18 13:47:03 +0100

    testing regex

--- app/app.py
+++ app/app.py
@@ -25,10 +25,11 @@ def getPayload(request):
     )
     m = re.match(regex, str(request))
     if m:
-        ip = re.findall(r'[0-9]+(?:\.[0-9]+){3}', m.group(0))
-        port = re.findall(r'(?:[0-9]{1,5})', m.group(0))
-        path = re.findall(r'(?:\/[.]{1,})', m.group(0))
-        pprint.pprint(ip, port, path)
+        connect = []
+        connect['ip'] = re.findall(r'[0-9]+(?:\.[0-9]+){3}', m.group(0))
+        connect['port'] = re.findall(r'(?:[0-9]{1,5})', m.group(0))
+        connect['path'] = re.findall(r'(?:\/[.]{1,})', m.group(0))
+        pprint.pprint(connect)
 
 
 
