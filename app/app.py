decd7298f9758d646fa79ef02cf2e47a730ad696
wirehack7 <markusfritz87@gmail.com>
2021-12-18 15:29:55 +0100

    disabled debug

--- app/app.py
+++ app/app.py
@@ -171,4 +171,4 @@ def homepage():
 
 if __name__ == '__main__':
     config = read_conf()
-    app.run(debug=True, host=config['DEFAULT']['ip'], port=int(config['DEFAULT']['port']))
+    app.run(debug=False, host=config['DEFAULT']['ip'], port=int(config['DEFAULT']['port']))
